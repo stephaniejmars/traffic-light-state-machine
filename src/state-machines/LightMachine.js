4dee8ca89031ace4e97ec8fe568992fa50f6c6e4
Stephanie <stephaniejmars@gmail.com>
2020-05-21 15:04:37 -0700

    Xstate lightMachine functional

--- src/state-machines/LightMachine.js
+++ src/state-machines/LightMachine.js
@@ -1,8 +1,27 @@
 import { createMachine, interpret } from 'xstate';
 
-const lightMachine = createMachine({
+const lightMachine = Machine({
   // Your state machine blueprint here
-
+  
+  id: 'lightToggle',
+  initial: 'red', 
+  states: {
+    red: {
+      on: {
+        changeLight: 'green'
+      },
+    }, 
+    green: {
+      on:{
+        changeLight: 'yellow'
+      },
+    },
+    yellow: {
+      on: {
+        changeLight: 'red', 
+      },
+    },
+  },
 });
 
 
